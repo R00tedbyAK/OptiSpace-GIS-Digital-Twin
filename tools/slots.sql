a113b8f007443a4d01b31ae386b6b4c9831d6df6
JithuMon10 <jva06102006@gmail.com>
2026-01-31 12:32:33 +0530

    feat: add initial parking slot data to `slots.sql`

--- tools/slots.sql
+++ tools/slots.sql
@@ -1,2 +1,4 @@
 -- OptiSpace Generated Slots SQL --
 INSERT INTO parking_slots (slot_id, slot_name, status, lat, lng, zone_type) VALUES (NULL, 'ENTRANCE', 'free', 8.48853378086417, 76.92194851273933, 'general');
+INSERT INTO parking_slots (slot_id, slot_name, status, lat, lng, zone_type) VALUES (NULL, 'A-1', 'free', 8.48768507001593, 76.92364641376331, 'general');
+INSERT INTO parking_slots (slot_id, slot_name, status, lat, lng, zone_type) VALUES (NULL, 'A-2', 'free', 8.487380008760905, 76.92361958800721, 'general');
